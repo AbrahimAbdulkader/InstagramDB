4c132efd3ffaf238906bfbf08571b5085c6ee6e0
Abrahim Abdulkader <abrahimabdulkader370@gmail.com>
2022-05-08 20:15:28 +0000

    Photos Table

--- ig_clone.sql
+++ ig_clone.sql
@@ -6,6 +6,14 @@ CREATE TABLE users (
 );
 
 
+CREATE TABLE photos (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    image_url VARCHAR(255) NOT NULL,
+    user_id INT NOT NULL,
+    created_at TIMESTAMP DEFAULT NOW(), 
+    FOREIGN KEY(user_id) REFERENCES users(id)
+); 
+
 
           
         
