1c1a4c359a847219f525d5f0c4978e92ffce1a3f
Abrahim Abdulkader <abrahimabdulkader370@gmail.com>
2022-05-13 17:42:23 +0000

    Solutions PT2

--- solutions.sql
+++ solutions.sql
@@ -21,5 +21,64 @@ ORDER BY total DESC;
      FROM users
      LEFT JOIN photos
          ON users.id = photos.user_id
-    WHERE photos.id IS NULL;
+    WHERE photos.id IS NULL; 
+    
+    
+    
+    -- 4. Identify most popular photo(and user who created it) 
+    
+    SELECT photos.id, photos.image_url,
+    COUNT(*) AS total
+    FROM photos
+    INNER JOIN likes 
+         ON likes.photo_id = photos.id
+    INNER JOIN users   
+        ON photos.user_id = users.id 
+    GROUP BY photos.id 
+    ORDER BY total DESC
+    LIMIT 1;
+    
+    
+    
+    -- 5. How many times does the average user post? 
+    -- Calculate avg number of photos per user
+    
+    SELECT (SELECT COUNT(*) FROM photos) / (SELECT COUNT(*) FROM users) AS avg;
+    
+    
+    -- 6. What are the top 5 most commonly used hashtags?
+    
+    SELECT 
+        tags.tag_name, 
+        COUNT(*) as total 
+    FROM photo_tags 
+    JOIN tags 
+        ON photo_tags.tag_id = tags.id 
+        GROUP BY tags.id 
+        ORDER BY total DESC
+        LIMIT 5;
+        
+        
+        
+        -- 7. Finding Bots - users who have liked every single photo_tags
+    
+    # SELECT 
+    #     username, 
+    #     COUNT(*) AS num_likes 
+    # FROM users
+    # INNER JOIN likes   
+    #     ON users.id = likes.user_id
+    # GROUP BY likes.user_id 
+    # HAVING num_likes = (SELECT COUNT(*) FROM photos);
+        
+        
+        
+    
+    
+    
+   
+    
+    
+    
+    
      
